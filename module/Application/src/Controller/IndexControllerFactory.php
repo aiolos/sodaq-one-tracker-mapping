70e85b65bdaf6f290854c33bf755b07dea8172c4
Henri de Jong <henridejong@gmail.com>
2017-08-02 00:42:45 +0200

    Style fix

--- module/Application/src/Controller/IndexControllerFactory.php
+++ module/Application/src/Controller/IndexControllerFactory.php
@@ -10,7 +10,7 @@ class IndexControllerFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $config = $container->get('config');
-        $em = $container->get(EntityManager::class);
-        return new IndexController($em, $config);
+        $entityManager = $container->get(EntityManager::class);
+        return new IndexController($entityManager, $config);
     }
 }
