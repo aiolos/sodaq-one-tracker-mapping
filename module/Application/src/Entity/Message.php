240c8351a64bb48006ce522600d2275c6d578514
Henri de Jong <henri.de.jong@infi.nl>
2017-08-03 19:25:55 +0200

    Remove the actual duplicate

--- module/Application/src/Entity/Message.php
+++ module/Application/src/Entity/Message.php
@@ -289,34 +289,4 @@ class Message
 
         return $message;
     }
-
-    /**
-     * Decode the raw payload in the message,
-     * see http://forum.sodaq.com/t/interpreting-payload-data-from-the-sodaqone-universaltracker/374
-     * for more information
-     * @param $rawPayload
-     * @return array
-     */
-    public static function decodeRawPayload($rawPayload)
-    {
-        $parsed = [];
-        $hex = bin2hex(base64_decode($rawPayload));
-        // Currently we only support the sodaq one tracker payload, which sends only 21 bytes
-        if (strlen($hex) !== 42) {
-            return null;
-        }
-
-        $parsed['epoch'] = hexdec(substr($hex, 6, 2) . substr($hex, 4, 2) . substr($hex, 2, 2) . substr($hex, 0, 2));
-        $parsed['batvolt'] = (3000 + 10 * hexdec(substr($hex, 8, 2))) / 1000;
-        $parsed['boardtemp'] = hexdec(substr($hex, 10, 2));
-        $parsed['lat'] = hexdec(substr($hex, 18, 2) . substr($hex, 16, 2) . substr($hex, 14, 2) . substr($hex, 12, 2)) / 10000000;
-        $parsed['lon'] = hexdec(substr($hex, 26, 2) . substr($hex, 24, 2) . substr($hex, 22, 2) . substr($hex, 20, 2)) / 10000000;
-        $parsed['alt'] = hexdec(substr($hex, 30, 2) . substr($hex, 28, 2));
-        $parsed['speed'] = hexdec(substr($hex, 34, 2) . substr($hex, 32, 2));
-        $parsed['course'] = hexdec(substr($hex, 36, 2));
-        $parsed['numsat'] = hexdec(substr($hex, 38, 2));
-        $parsed['fix'] = hexdec(substr($hex, 40, 2));
-
-        return $parsed;
-    }
 }
